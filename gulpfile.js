7bd560b746a1d752e89047fec6efab69218380b3
KO. Mattsson <karloskar@ponty.se>
2017-05-29 11:24:25 +0200

    Added php version str to bump task.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require('gulp');
 const gutil = require('gulp-util');
 const zip = require('gulp-zip');
 const bump = require('gulp-bump');
+const vMap = require('vinyl-map');
+const semver = require('semver');
 
 const pkg = require('./package.json');
 
@@ -9,10 +11,22 @@ gulp.task('bump', () => {
   gulp.src(['package.json'])
   .pipe(bump())
   .pipe(gulp.dest('.'))
+
   gulp.src([
     'ponty-connector/ponty-connector.php'
   ])
   .pipe(bump())
+  .pipe(vMap((contents, filename) => {
+    // special special for the php:define version str
+    let new_content = contents.toString();
+    const re0 = new RegExp(/PNTY_VERSION', '(\d+.\d+.\d+)'/)
+    const version_string = new_content.match(re0);
+    const version_part = version_string[1];
+    const new_version_part = semver.inc(version_part, 'patch');
+    const new_version_string = `PNTY_VERSION', '${new_version_part}'`;
+    new_content = new_content.replace(re0, new_version_string);
+    return new_content;
+  }))
   .pipe(gulp.dest('ponty-connector'))
 });
 
