2703e42d94c8c52ad603772ed338d093571d2a5c
KO. Mattsson <karloskar@ponty.se>
2017-05-29 11:25:57 +0200

    Version 1.0.2

--- ponty-connector/ponty-connector.php
+++ ponty-connector/ponty-connector.php
@@ -3,11 +3,11 @@
     Plugin Name: Ponty Connector
     Description: Plugin used to connect Ponty Recruitment System with your site
     Author: KO. Mattsson with contributions from Andreas Lagerkvist
-    Version: 1.0.1
+    Version: 1.0.2
     Author URI: https://ponty.se
 */
 # The name of the custom post types
-define('PNTY_VERSION', '1.0.1');
+define('PNTY_VERSION', '1.0.2');
 define('PNTY_PTNAME', 'pnty_job');
 define('PNTY_PTNAME_SHOWCASE', 'pnty_job_showcase');
 
